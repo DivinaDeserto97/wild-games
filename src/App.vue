58828f5c7b76ddc7459358b5b55975ca0f12f0f6
Divina Deserto <117446023+DivinaDeserto97@users.noreply.github.com>
2023-05-15 09:08:31 +0200

    projekt name

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="navbar-desktop">
+    <!-- navbar desktop -->
     <nav class="navbar navbar-expand-md">
       <div class="container-fluid">
         <div class="row">
@@ -82,7 +83,171 @@
       </div>
     </nav>
   </div>
-<!--   <WelcomePadge /> -->
+  <div class="navbar-handy">
+    <!-- navbar händy -->
+    <nav class="navbar bg-body-tertiary fixed-top bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <i class="fa-solid fa-alien-8bit"></i>Wild-Games
+        </a>
+        <button
+          class="navbar-toggler btn btn-light"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNavbar"
+          aria-controls="offcanvasNavbar"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          class="offcanvas offcanvas-end"
+          tabindex="-1"
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+        >
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
+              <i class="fa-solid fa-alien-8bit"></i>Wild-Games
+            </h5>
+            <button
+              type="button"
+              class="btn-close btn btn-light"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Welcome</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Games</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Medien
+                </a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <i class="fa-brands fa-youtube"></i> Youtoube
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="https://discord.gg/K5tpBH67">
+                      <i class="fa-brands fa-discord"></i> Discorde
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown" style="display: none;">
+                <!--  style="display: none;" -->
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  User
+                </a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <i class="fa-solid fa-arrow-right"></i>
+                      <i class="fa-solid fa-user"></i> Login
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <i class="fa-solid fa-user-pen"></i> Registreiren
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  User
+                </a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <i class="fa-solid fa-user-slash"></i> Logout
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      <i class="fa-solid fa-user"></i> Account
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            <form class="d-flex mt-3" role="search">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button class="btn btn-outline-success" type="submit">
+                <i class="fa-solid fa-magnifying-glass"></i>
+              </button>
+            </form>
+            <form class="d-flex mt-3">
+              <select
+                class="form-select form-select-lg mb-3"
+                aria-label=".form-select-lg example"
+              >
+                <option selected>Langue</option>
+                <option value="en">English</option>
+                <option value="de">German</option>
+              </select>
+              <button class="btn btn-outline-success" type="submit">
+                <i class="fa-regular fa-language"></i>
+              </button>
+            </form>
+            <form>
+              <div class="form-check form-switch">
+                <input
+                  @click="toggleTheme"
+                  class="form-check-input"
+                  type="checkbox"
+                  role="switch"
+                  id="flexSwitchCheckChecked"
+                  checked
+                />
+                <label class="form-check-label" for="flexSwitchCheckChecked"
+                  >Dark Light</label
+                >
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+  <!--   <WelcomePadge /> -->
 </template>
 
 <script>
@@ -106,6 +271,7 @@ export default {
     setTheme() {
       const buttons = document.querySelectorAll(".btn");
       const navbar = document.querySelector(".navbar");
+      const offcanvas = document.querySelector(".offcanvas");
 
       if (this.isDark) {
         buttons.forEach((button) => {
@@ -114,6 +280,10 @@ export default {
         });
         navbar.classList.remove("navbar-light", "bg-light");
         navbar.classList.add("navbar-dark", "bg-dark");
+        offcanvas.classList.remove("text-bg-dark");
+        offcanvas.classList.add("text-bg-light");
+        document.body.classList.remove("bg-light");
+        document.body.classList.add("bg-dark");
       } else {
         buttons.forEach((button) => {
           button.classList.remove("btn-dark");
@@ -121,11 +291,40 @@ export default {
         });
         navbar.classList.remove("navbar-dark", "bg-dark");
         navbar.classList.add("navbar-light", "bg-light");
+        offcanvas.classList.remove("text-bg-light");
+        offcanvas.classList.add("text-bg-dark");
+        document.body.classList.remove("bg-dark");
+        document.body.classList.add("bg-light");
       }
     },
+
     toggleTheme() {
       this.isDark = !this.isDark;
     },
   },
 };
 </script>
+
+<style>
+/* Navbar Desktop */
+.navbar-desktop {
+  display: none; /* Versteckt das Element auf allen Geräten */
+}
+
+@media (min-width: 768px) {
+  .navbar-desktop {
+    display: flex !important; /* Zeigt das Element auf Geräten mit einer Breite von 768px oder mehr an */
+  }
+}
+
+/* Navbar Händy */
+.navbar-handy {
+  display: flex; /* Zeigt das Element auf allen Geräten an */
+}
+
+@media (min-width: 768px) {
+  .navbar-handy {
+    display: none; /* Versteckt das Element auf Geräten mit einer Breite von 768px oder mehr */
+  }
+}
+</style>
